ed6c273bce5ce231db49168768440c61702b6e05
Danjo <atze.danjo@gmail.com>
2020-05-16 19:22:09 +0200

    Cap container width at 1280px

--- web/middleware.py
+++ web/middleware.py
@@ -8,9 +8,6 @@ class SelectedChannelMiddleware(MiddlewareMixin):
 
 	def process_request(self, request):
 		if request.user.is_authenticated:
-			print('USER IS AUTHENTICATED')
 			channel = get_channel(request.user)
 			request.channel = channel
-			print(request.channel)
-		else:
-			print('ANONYMOUS USER')
+			
