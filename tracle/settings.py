1c2cd502d164411b2f021276cf3a0bf1de4cafab
Danjo <atze.danjo@gmail.com>
2020-08-06 17:27:41 +0200

    Display upload date as '... ago'

--- tracle/settings.py
+++ tracle/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'django.contrib.humanize',
 
     'backend.apps.BackendConfig',
     'web.apps.WebConfig',
