eb7c398299a5fea91fe2f15a1539c1eae49ce8d6
Danjo <atze.danjo@gmail.com>
2021-03-04 01:59:13 +0100

    Create events for channel feed on uploads and new comments

--- backend/signals.py
+++ backend/signals.py
@@ -3,6 +3,8 @@ import base64, fixedint, re
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
+from actstream import action
+
 from .models import Video, Channel, Image, Comment, Notification
 
 @receiver(post_save, sender=Channel)
@@ -24,6 +26,7 @@ def generate_watch_id(sender, instance, created, **kwargs):
             del instance._dirty
     else:
         if not instance.subs_notified and instance.published and instance.transcode_status == instance.TranscodeStatus.DONE and instance.visibility == instance.VisibilityStatus.PUBLIC:
+            action.send(instance.channel, verb='uploaded', action_object=instance)
             subs = [ sub.from_channel.user for sub in instance.channel.subscriptions.all() ]
             for sub in subs:
                 Notification.objects.create(notification_type=Notification.NotificationType.VIDEO, actor=instance.channel, action_object=instance, target_object=instance.channel, recipient=sub)
@@ -46,6 +49,7 @@ def delete_video_files(sender, instance, using, **kwargs):
 @receiver(post_save, sender=Comment)
 def send_comment_notification(sender, instance, created, **kwargs):
     if created:
+        action.send(instance.author, verb='commented', action_object=instance, target=instance.video)
         if instance.author != instance.video.channel:
             Notification.objects.create(notification_type=Notification.NotificationType.COMMENT, actor=instance.author, action_object=instance, target_object=instance.video, recipient=instance.video.channel.user)
         tags = re.findall('@\(\w+\)\[([aA-zZ0-9-_]+)\]', instance.text)
