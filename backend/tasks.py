bf619ff82e3ddc81054cd23e3566b85b35bb455c
Danjo <atze.danjo@gmail.com>
2021-09-12 01:38:00 +0200

    Fix: Transcode fails if original video already uploaded to bunny storage

--- backend/tasks.py
+++ backend/tasks.py
@@ -36,5 +36,10 @@ def bunnyvideo_upload_task(bunny_video=None):
 	vobj = vapi.create_video(bunny_video.video.watch_id)
 	bunny_video.bunny_guid = vobj['guid']
 	bunny_video.save()
-	vapi.upload_video(bunny_video.bunny_guid, bunny_video.video.uploaded_file.path)
-	bunny_video.video.transfer_files()
+	try:
+		local_path = bunny_video.video.uploaded_file.path
+		vapi.upload_video(bunny_video.bunny_guid, local_path)
+		bunny_video.video.transfer_files()
+	except NotImplementedError:
+		url = f'https://storage.bunnycdn.com/{settings.BUNNYCDN["storage_zone_name"]}/{video.uploaded_file.name}'
+		vapi.fetch_video(bunny_video.bunny_guid, url, fetch_headers={'headers' : {'AccessKey' : settings.BUNNYCDN['access_token']}})
