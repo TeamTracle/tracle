dd06817074c2f2e497d749184453fd37b28e6932
Danjo <atze.danjo@gmail.com>
2022-01-31 03:39:29 +0100

    Remove SilkyMiddleware (accidentally added in prev commit)

--- tracle/settings.py
+++ tracle/settings.py
@@ -66,7 +66,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'waffle.middleware.WaffleMiddleware',
-    'silk.middleware.SilkyMiddleware',
 ]
 
 SESSION_ENGINE = 'qsessions.backends.db'
